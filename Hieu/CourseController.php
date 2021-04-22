bed10e6c0f827b17c66317ba40c80d5054f0f279
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2021-04-22 10:58:41 +0700

    lmao

--- Hieu/CourseController.php
+++ Hieu/CourseController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Exam_result;
 use App\Models\Teacher;
+use App\Models\Visitor;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\isFalse;
 
 class CourseController extends Controller
 {
@@ -29,7 +32,14 @@ class CourseController extends Controller
     public function entrance_exam() {
         return view('front.entrance_exam.entrance_exam');
     }
-    public function search_exam() {
-        return view('front.entrance_exam.search_exam');
+    public function search_exam(Request $request) {
+        $search = $request->search;
+
+        $entrance_exam_results = Visitor::where('id','LIKE', '%' . $search . '%')
+            ->get()->first();
+
+
+        return view('front.entrance_exam.search_exam',compact('entrance_exam_results'));
     }
+
 }
